0d3c12562f9c90317938789e30afa17c8743e37f
Emil Sharkov <emosharkov@gmail.com>
2024-01-20 20:25:17 -0800

    added kubernetes deployment and load balancer service

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM python:3.11-slim-bookworm
 
 WORKDIR /usr/src/app
 
-COPY . .
+# COPY . .
+COPY api.py requirements.txt .
 
 RUN apt-get update
-RUN apt-get install -y python3-dev gcc-11 g++-11 build-essential
+# RUN apt-get install -y python3-dev gcc-11 g++-11 build-essential
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["python","api.py"]
--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 import uvicorn
 from fastapi import Request, FastAPI
-from llama_cpp import Llama
+# from llama_cpp import Llama
 
 app = FastAPI()
-LLM = Llama(model_path="./llama-2-7b-chat.ggmlv3.q8_0.bin")
+# LLM = Llama(model_path="./llama-2-7b-chat.ggmlv3.q8_0.bin")
 
 @app.get("/")
 async def index():
@@ -13,12 +13,12 @@ async def index():
 async def health():
    return {"Health": "Up"}
 
-@app.post("/ask")
-async def prompt(req: Request):
-   body = await req.json()
-   prompt = body['question']
-   answer = LLM(prompt)["choices"][0]["text"]
-   return {"answer": answer}
+# @app.post("/ask")
+# async def prompt(req: Request):
+#    body = await req.json()
+#    prompt = body['question']
+#    answer = LLM(prompt)["choices"][0]["text"]
+#    return {"answer": answer}
 
 if __name__ == "__main__":
-   uvicorn.run("api:app", host="127.0.0.1", port=80, reload=False)
+   uvicorn.run("api:app", host="0.0.0.0", port=80, reload=False)
